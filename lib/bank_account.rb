7c7200d3b60473bf2a411091e03166bd5c571677
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-12-07 18:30:07 -0600

    commit tree 2e60813eb9279f35e4dfdcd01d38ed2ed3d233fa

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
 
+    attr_accessor :balance, :status
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(deposit)
+        self.balance += deposit
+    end
+
+    def withdrawal(withdrawal)
+        self.balance -= withdrawal
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+
 end
